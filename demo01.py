d920440f99207189bf88f21cc2ee56189193e778
Xiehuaru <1712083022@qq.com>
2020-09-15 20:58:26 +0800

    first commit

---
+++ demo01.py
@@ -0,0 +1,18 @@
+# print("hello world");
+# """aaehfnajfjwef
+# gsdgsknksv
+# dflng
+# dfgslkgs
+# dnfgkjns"""
+# print(2.33);
+
+# print(int("14",8));
+
+# print(int("100"));
+
+# complex(2,1)
+
+import time
+import math
+import numpy as np
+
